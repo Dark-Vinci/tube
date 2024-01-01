51aa4275e208651f0aee7391491fdebb5a09566f
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-01-01 16:05:43 +0100

    chore:

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,2 +1,15 @@
-#!/bash/sh
+#!/bin/sh
 
+echo "Syncing changes in working directory to staging";
+
+cd ..
+
+git add .
+
+echo "Committing changes"
+
+git commit -am "chore: $1"
+
+echo "Pushing to github"
+
+git push -u origin main

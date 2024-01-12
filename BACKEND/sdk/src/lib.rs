e4b8a08880648cc21116aa65cd09bc60dd650b90
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-01-12 20:35:40 +0100

    chore: more update needs to come up

--- BACKEND/sdk/src/lib.rs
+++ BACKEND/sdk/src/lib.rs
@@ -1,20 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 pub mod errors;
 pub mod helpers;
 pub mod constants;
 pub mod generated_proto_rs;
 pub mod models;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}

32275433dd632a1a1f52fa5880f4f0f5dd375014
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-04-01 13:37:12 +0100

    chore: more update

--- BACKEND/posts/target/debug/deps/sdk-09b220a804a6f727.d
+++
@@ -1,7 +0,0 @@
-/Users/ademolaolutomiwaabraham/Desktop/allcodes/work-go/src/rust-clones/youtube-clone/BACKEND/posts/target/debug/deps/sdk-09b220a804a6f727.rmeta: /Users/ademolaolutomiwaabraham/Desktop/allcodes/work-go/src/rust-clones/youtube-clone/BACKEND/sdk/src/lib.rs
-
-/Users/ademolaolutomiwaabraham/Desktop/allcodes/work-go/src/rust-clones/youtube-clone/BACKEND/posts/target/debug/deps/libsdk-09b220a804a6f727.rlib: /Users/ademolaolutomiwaabraham/Desktop/allcodes/work-go/src/rust-clones/youtube-clone/BACKEND/sdk/src/lib.rs
-
-/Users/ademolaolutomiwaabraham/Desktop/allcodes/work-go/src/rust-clones/youtube-clone/BACKEND/posts/target/debug/deps/sdk-09b220a804a6f727.d: /Users/ademolaolutomiwaabraham/Desktop/allcodes/work-go/src/rust-clones/youtube-clone/BACKEND/sdk/src/lib.rs
-
-/Users/ademolaolutomiwaabraham/Desktop/allcodes/work-go/src/rust-clones/youtube-clone/BACKEND/sdk/src/lib.rs:

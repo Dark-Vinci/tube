413bc24f17c834c027b55d8705756ba7a2db5545
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-05-24 10:12:23 +0100

    chore: more update

--- BACKEND/auth/src/application/ban.rs
+++ BACKEND/auth/src/application/ban.rs
@@ -29,3 +29,62 @@ impl Ban for App {
         todo!()
     }
 }
+
+#[cfg(test)]
+mod test {
+    use {super::App, crate::application::traits::Others, uuid::Uuid};
+    use crate::application::traits::Ban;
+
+    async fn get_app() -> App {
+        return App::new(Default::default()).await.unwrap();
+    }
+
+    // create stub service for Redis, DB, RabbitMQ, use it to create the App instance
+    #[tokio::test]
+    async fn ban_user_test() {
+        let a = get_app().await;
+        let res = a.ban_user().await;
+
+        assert_eq!(res, ());
+    }
+
+    #[tokio::test]
+    async fn ban_channel_test() {
+        let a = get_app().await;
+        let res = a.unban_channel().await;
+
+        assert_eq!(res, ());
+    }
+
+    #[tokio::test]
+    async fn unban_user_test() {
+        let a = get_app().await;
+        let res = a.unban_user().await;
+
+        assert_eq!(res, ());
+    }
+
+    #[tokio::test]
+    async fn unban_channel_test() {
+        let a = get_app().await;
+        let res = a.unban_channel().await;
+
+        assert_eq!(res, ());
+    }
+
+    #[tokio::test]
+    async fn ban_short_test() {
+        let a = get_app().await;
+        let res = a.unban_short().await;
+
+        assert_eq!(res, ());
+    }
+
+    #[tokio::test]
+    async fn unban_short_test() {
+        let a = get_app().await;
+        let res = a.unban_short().await;
+
+        assert_eq!(res, ());
+    }
+}

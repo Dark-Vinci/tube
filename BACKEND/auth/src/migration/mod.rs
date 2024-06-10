8dfbbb1a5319cce191d9293f8076c76bf897a351
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-06-10 04:52:45 +0100

    chore: see if it works

--- BACKEND/auth/src/migration/mod.rs
+++ BACKEND/auth/src/migration/mod.rs
@@ -2,4 +2,5 @@ mod m20240108_000001_create_cake_table;
 mod m20240115_184133_migration;
 mod m20240116_024655_make_we_see_ooo;
 mod m20240502_034128_update_user;
+mod m20240610_035127_melon_melon;
 pub mod migrator;
--- scripts/migration.sh
+++ scripts/migration.sh
@@ -1,27 +1,38 @@
 #!/bin/sh
 
+if [[ -z $1 && -z $2 ]]; then
+    echo "Provide service name and migration name"
+    exit
+fi
+
+if [[ -z $1 ]]; then
+    echo "Service name not provided"
+    exit
+fi
+
+if [[ -z $2 ]]; then
+    echo "migration name is not provided"
+    exit
+fi
+
 case $1 in
   "auth")
     echo "about to cd into auth service to generate migration"
-    # shellcheck disable=SC2164
     cd ../BACKEND/auth/src
     sea-orm-cli migrate generate "$2";;
 
   "reactions")
     echo "about to cd into reactions service to generate migration"
-    # shellcheck disable=SC2164
     cd ../BACKEND/reactions/src
     sea-orm-cli migrate generate "$2";;
 
   "posts")
     echo "about to cd into posts service to generate migration"
-    # shellcheck disable=SC2164
     cd ../BACKEND/posts/src
     sea-orm-cli migrate generate "$2";;
 
   "utils")
     echo "about to cd into utils service to generate migration"
-    # shellcheck disable=SC2164
     cd ../BACKEND/utils/src
     sea-orm-cli migrate generate "$2";;
 
